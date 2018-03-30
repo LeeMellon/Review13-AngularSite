d7c2bf040c991fc8053edd9159571ac909f9f868
Ian <iangoodrich@hotmail.com>
2018-03-30 14:38:45 -0700

    re styled new article inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { FormsModule }  from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NewArticleComponent } from './new-article/new-article.component';
 import { SubCatComponent } from './sub-cat/sub-cat.component';
+import { ArticleEditComponent } from './article-edit/article-edit.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NewArticleComponent,
-    SubCatComponent
+    SubCatComponent,
+    ArticleEditComponent
   ],
   imports: [
     BrowserModule,
