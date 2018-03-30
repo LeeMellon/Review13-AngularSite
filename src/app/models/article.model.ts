6e164d07886e056b79f9e930fd0c402e4fe92f63
Ian <iangoodrich@hotmail.com>
2018-03-30 10:10:10 -0700

    refactored constructor, added blurb and id parameters.

--- src/app/models/article.model.ts
+++ src/app/models/article.model.ts
@@ -1,3 +1,3 @@
 export class Article{
-  constructor(public title: string, public body: string, public img: string, public author: string)
+  constructor(public title: string, public author: string, public blurb: string, public body: string, public img: string, public id: number)
 }
