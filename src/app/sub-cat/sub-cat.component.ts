ca3e666211f411180bb3951efbe5fd969ec67cad
Ian <iangoodrich@hotmail.com>
2018-03-30 10:17:15 -0700

    add sub-cat module.

---
+++ src/app/sub-cat/sub-cat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-cat',
+  templateUrl: './sub-cat.component.html',
+  styleUrls: ['./sub-cat.component.css']
+})
+export class SubCatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
