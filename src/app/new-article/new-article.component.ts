0c21e6212969b8b3db3ec813fb95e9b2165be957
Ian <iangoodrich@hotmail.com>
2018-03-30 15:55:13 -0700

    add jquery effects to show/hide new article field from nave bar

--- src/app/new-article/new-article.component.ts
+++ src/app/new-article/new-article.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Article } from '../models/article.model';
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-new-article',
   templateUrl: './new-article.component.html',
@@ -11,8 +13,12 @@ export class NewArticleComponent {
   create(title: string, author: string, blurb: string, body: string, img: string, alt: string, id: string){
     let newArticle = new Article(title, author, blurb, body, img, alt, parseInt(id));
     this.createSender.emit(newArticle);
+    $(".new-article-container").fadeOut(300,"swing");
   }
 
+  showNew(){
+    $(".new-article-container").fadeIn(300, "swing");
+  }
   constructor() { }
 
 
