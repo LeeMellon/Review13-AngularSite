83d7ccd11996f8a6181903cf3d2143ff992b6619
Ian <iangoodrich@hotmail.com>
2018-03-30 10:07:29 -0700

    create mk1 input form for manual input of articles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule }  from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NewArticleComponent } from './new-article/new-article.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewArticleComponent
   ],
   imports: [
     BrowserModule,
