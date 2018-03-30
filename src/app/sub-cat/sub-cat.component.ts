397cd8d5c3eda99a5acd629ecf9332afc7ebac71
Ian <iangoodrich@hotmail.com>
2018-03-30 10:40:36 -0700

    imported Input, Output, EventEmitter

--- src/app/sub-cat/sub-cat.component.ts
+++ src/app/sub-cat/sub-cat.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-sub-cat',
   templateUrl: './sub-cat.component.html',
   styleUrls: ['./sub-cat.component.css']
 })
-export class SubCatComponent implements OnInit {
+export class SubCatComponent {
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
