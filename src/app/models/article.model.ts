6b9bee5ce13308ea98101651be9c2f25dc718d70
Ian <iangoodrich@hotmail.com>
2018-03-30 10:36:34 -0700

    add alt parameter to article class

--- src/app/models/article.model.ts
+++ src/app/models/article.model.ts
@@ -1,3 +1,3 @@
 export class Article{
-  constructor(public title: string, public author: string, public blurb: string, public body: string, public img: string, public id: number)
+  constructor(public title: string, public author: string, public blurb: string, public body: string, public img: string, public alt: string, public id: number)
 }
